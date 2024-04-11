944e2d3d682f78a6d8e8a44405ec43bdc670572e
Bogdan Zotov <zotov.bogdan2017@yandex.ru>
2024-04-12 00:31:27 +0300

    Init

--- src/Components/textContent/txtPostCon.tsx
+++ src/Components/textContent/txtPostCon.tsx
@@ -11,10 +11,14 @@ const TxtPostCon: React.FC<TxtPostProps> = ({ TxtPropArr }) => {
   return (
     <div className="txtCont" style={{ backgroundColor: TxtPropArr?.txtBackColor }}>
       <div className="txtContTxt">
-
+        <span>ffffffffffffffffffffffffffffffffffffff</span>
       </div>
-      <button></button>
-      <div className="Hashtag"></div>
+      <button className="BackButton rounded-button">
+        <span className="left-semicircle"></span>
+        <span className="button-content">Hide</span>
+        <span className="right-semicircle"></span>
+      </button>
+      <div className="Hashtag">fdsfdsf</div>
     </div>
   )
 }
