5bd1bcc67810cc069ca26d92b2e9b2f720a9a7eb
Bogdan Zotov <zotov.bogdan2017@yandex.ru>
2024-04-25 01:08:32 +0300

    Init

--- src/Components/Filler/Filler.tsx
+++ src/Components/Filler/Filler.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Cirucs from "./../../Resources/Images/CircusPhon.jpg";
 import "./Filler.css";
 import { JokeArr, JokeMock } from "../../Mock/JokeMock";
+import JokeBegining from "./../../Resources/Music/JokeBegining.mp3"
+import JokeLaugh from "./../../Resources/Music/JokeEnd.mp3"
 
 interface FillerProps {
   JokeNumber: number;
@@ -9,20 +11,26 @@ interface FillerProps {
 
 const Filler: React.FC<FillerProps> = ({ JokeNumber }) => {
 
+  const MusicDelay = 520;
 
   const curJoke: JokeMock | undefined = JokeArr.find(Content => Content.ID === JokeNumber);
 
   const [curOpOpener1, setCurOpOpener1] = useState<boolean>(false)
   const [curOpOpener2, setCurOpOpener2] = useState<boolean>(false)
 
+  const JokeBegin = new Audio(JokeBegining)
+  const JokeEnd = new Audio(JokeLaugh) 
+
   const FirstOpen = () => {
-    //music
+    JokeBegin.play()
     setCurOpOpener1(true)
   }
   const SecondOpen = () => {
-    //music
     setCurOpOpener2(true)
-    //music
+    JokeEnd.play()
+  }
+  const DelayedSeconOpen = () => {
+    setTimeout(SecondOpen, MusicDelay);
   }
 
   return (
@@ -31,7 +39,7 @@ const Filler: React.FC<FillerProps> = ({ JokeNumber }) => {
       <div className="CircusContent">
       <button className={`FirstOpener ${curOpOpener1? "Defused" : "notDefused"}`} onClick={FirstOpen}>🎪</button>
         <div className={`CircusTxt ${curOpOpener1? "notDefused" : "Defused"}`}>{curJoke?.questTxt}</div>
-        <button className={`SecondOpener ${curOpOpener1? "notDefused" : "Defused"} ${curOpOpener2? "Defused" : "notDefused"}`} onClick={SecondOpen}>🤡</button>
+        <button className={`SecondOpener ${curOpOpener1? "notDefused" : "Defused"} ${curOpOpener2? "Defused" : "notDefused"}`} onClick={DelayedSeconOpen}>🤡</button>
         <div className={`CircusTxtAnswer ${curOpOpener2? "notDefused" : "Defused"}`}>{curJoke?.answerTxt}</div>
       </div>
     </div>
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,18 @@ import "./Footer.css"
 function Footer() {
 
   return (
-    <div>
+    <div className="FooterContainer">
+      <div className="Footer-Wrapper">
+        <div className="Footer">
+          <div className="FooterTxt">
+            <h1>Балдёжное отделение 61</h1>
+            Сервис для чила и кайфа в сфере высшего образования
+          </div>
+          <div className="CopyRight">
+          ©️ Балдёжное отделение 61. Сайт содержит материалы, охраняемые от душнил и скуфов.
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
