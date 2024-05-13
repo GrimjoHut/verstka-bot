6c05cc54af15efad4d162bb394a819d384faa2cc
GrimjoHut <zotov.bogdan2019@yandex.ru>
2024-05-13 21:17:20 +0300

    init

--- src/Components/ExecutionComponents/LongWay/LongWay.tsx
+++ src/Components/ExecutionComponents/LongWay/LongWay.tsx
@@ -1,22 +1,27 @@
 import "./LongWay.css";
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 interface LongWayProps {
   NowayNumber: number;
   BackStyle: string;
+  FillerNumb: number
 }
 
-const LongWay: React.FC<LongWayProps> = ({  NowayNumber, BackStyle }) => {
+const LongWay: React.FC<LongWayProps> = ({  NowayNumber, BackStyle, FillerNumb }) => {
+  
+  const [beerVisibility, setBeerVisibility] = useState<boolean>(true)
   
   const NoWayTxtArr = [
-    "АбуАбу",
-    "fdfdf",
-    "fdfdfd",
-    "Fdfdfdsf",
-    "АбуАбу",
-    "fdfdf",
-    "fdfdfd",
-    "Fdfdfdsf",
-    "АбуАбу",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
     "",
     "",
     "",
@@ -28,17 +33,23 @@ const LongWay: React.FC<LongWayProps> = ({  NowayNumber, BackStyle }) => {
     "",
   ];
 
-
+  useEffect (() => 
+  {
+    if ((NowayNumber == 2) && (FillerNumb == 0)) 
+      setBeerVisibility(true)
+      else setBeerVisibility(false)
+  })
   return (
     <div className={`LongWayContainer ${BackStyle}`}>
      <div className="PhraseContainer">
-      <span>{NoWayTxtArr[17-9-NowayNumber*3]}</span>
+      <span>{NoWayTxtArr[17-9+FillerNumb*3-NowayNumber*3]}</span>
      </div>
      <div className="PhraseContainer">
-      <span>{NoWayTxtArr[17-9-NowayNumber*3-1]}</span>
+      <span>{NoWayTxtArr[17-9+FillerNumb*3-NowayNumber*3-1]}</span>
+      <span style={{visibility: beerVisibility? "visible" : "hidden"}}>Ну ладно уже, попей пива <a href="">🍺</a></span>
      </div>
      <div className="PhraseContainer">
-      <span>{NoWayTxtArr[17-9-NowayNumber*3-2]}</span>
+      <span>{NoWayTxtArr[17-9+FillerNumb*3-NowayNumber*3-2]}</span>
      </div>
     </div>
   );
--- src/Pages/ExecutionPage/Execution.tsx
+++ src/Pages/ExecutionPage/Execution.tsx
@@ -5,14 +5,22 @@ import "./Execution.css"
 
 function Execution() {
 
+    
     const [FillerNumb, setFillerNumb] = useState<number>(0)
+
+    const DownButton = () => {
+        window.scrollBy(0, window.innerHeight*51)
+        if (FillerNumb < 3)
+        setFillerNumb(FillerNumb+1)
+    }
 
     return (
         <body>
-            <div className="ContainerBut"><ExHeader /><button className="ButtonRoll">FD</button></div>
-            <LongWay BackStyle="Dirt" NowayNumber={0}/>
-            <LongWay BackStyle="Half" NowayNumber={1}/>
-            <LongWay BackStyle="Hell" NowayNumber={2}/>
+            <div className="ContainerBut"><ExHeader /><button onClick={DownButton} className="ButtonRoll">FD</button></div>
+            <LongWay FillerNumb={FillerNumb} BackStyle="Dirt" NowayNumber={0}/>
+            <div style={{backgroundColor: "rgb(0, 0, 0)", height: "100vh"}}>{FillerNumb}</div>
+            <LongWay FillerNumb={FillerNumb}BackStyle="Half" NowayNumber={1}/>
+            <LongWay FillerNumb={FillerNumb} BackStyle="Hell" NowayNumber={2}/>
         </body>
     )
 };
